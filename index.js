b9846a6fdd364ffce189270954462048221f39f5
open-schnick <60597856+open-schnick@users.noreply.github.com>
2021-02-21 14:29:25 +0100

    added command execution

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const handler = require("serve-handler");
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
+const { exit } = require("process");
+const { exec } = require("child_process");
 
 // custom stuff
 const logger = require("./lib/logger");
-const { exit } = require("process");
 
 const app = express();
 const port = 4200;
@@ -12,20 +14,53 @@ const port = 4200;
 // config
 let configJson = null;
 
-/*app.use((req, res) => {
-  handler(req, res)
-    .then((res) => {
-      console.log("res " + res);
-    })
-    .catch((err) => console.log(err));
-  return "no";
-});
-*/
-
 app.get("*", (req, res) => {
   // check authentication.
-  logger("got request.");
-  handler(req, res, { public: configJson.virtualFolderName });
+  if (req.url.includes(".log")) {
+    let foundFileName = req.url.split("/");
+    if (foundFileName.length != 2) {
+      logger("did not find foundFileName:", foundFileName);
+      res.sendStatus(404);
+    } else {
+      //split .log
+      foundFileName = foundFileName[1];
+      foundFileName = foundFileName.split(".log");
+
+      // compare with logs
+      let logs = configJson.logs;
+      logs = logs.filter((l) => l.name === foundFileName[0]);
+      if (logs != null && logs.length == 1) {
+        // check cache
+        // execute command
+        currentLog = logs[0];
+        logger("foundCommand", currentLog.name);
+        if (currentLog.command != null && currentLog.command !== "")
+          exec(currentLog.command, (err, stdout, stderr) => {
+            if (err == null) {
+              res.status(200).send(stdout);
+            } else {
+              logger(
+                "Encountered Exception while executing ",
+                currentLog.command
+              );
+              logger(err, stderr);
+              res
+                .status(500)
+                .send(
+                  "Encountered Exception while executing <b>" +
+                    currentLog.command +
+                    "</b></br>" +
+                    err
+                );
+            }
+          });
+      } else {
+        res.status(404).send("Could't find a logfile with this name");
+      }
+    }
+  } else {
+    handler(req, res, { public: configJson.virtualFolderName });
+  }
 });
 
 app.listen(port, () => {
@@ -50,9 +85,19 @@ app.listen(port, () => {
   // create fake folder
   if (!fs.existsSync(configJson.virtualFolderName)) {
     fs.mkdir(configJson.virtualFolderName, (err) =>
-      err ? logger("Couldn't create folder: " + err) : null
+      err ? logger("Couldn't create folder:", err) : null
     );
   }
 
-  logger("Server started on: http://localhost:" + port);
+  // create fake logfiles
+  if (configJson.logs && configJson.logs.length != null) {
+    configJson.logs.forEach((log) => {
+      // this does overwrite!
+      fs.writeFileSync(
+        path.join(configJson.virtualFolderName, log.name + ".log"),
+        "If you see this something fishy happend."
+      );
+    });
+  }
+  logger("Server started on: http://localhost:", port);
 });
