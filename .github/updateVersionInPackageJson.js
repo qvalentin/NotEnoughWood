c6545234e3db5ba091fc2afa32251b59a75d0ee6
open-schnick <60597856+open-schnick@users.noreply.github.com>
2021-03-15 17:56:33 +0100

    Added Actions to automate releases

---
+++ .github/updateVersionInPackageJson.js
@@ -0,0 +1,6 @@
+const fs = require("fs")
+const version = process.argv[2]
+
+const readPackage = JSON.parse(fs.readFileSync("package.json"))
+readPackage.version = version
+fs.writeFileSync("package.json", JSON.stringify(readPackage));
